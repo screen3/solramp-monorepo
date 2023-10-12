a6515d72272d941a88d6d50da84b54e11cf586c8
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-10-12 13:31:22 +0400

    Add OpenGraph support for Optimized Google saerch

--- platform/src/app/layout.js
+++ platform/src/app/layout.js
@@ -1,4 +1,3 @@
-import Navbar from "./components/HomePageLayout/Navbar";
 import { SatoshiFont } from "./fonts";
 import "./globals.css";
 import { Inter } from "next/font/google";
@@ -7,6 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
     title: "SolRamp: Accept crypto payment for non-crypto users on Solana",
+    metadataBase: new URL("https://solramp.com"),
     description:
         "Accept crypto payments from non-crypto users. Use our Solramp SDK on your website, available for Vuejs and Reactjs, on your WordPress site, and on your Shopify store.",
     keyword: [
@@ -22,30 +22,48 @@ export const metadata = {
         "Pay with USDT",
         "Pay with SOL",
     ],
-    icons: {
-        other: {
-            rel: "apple-touch-icon-precomposed",
-            url: "/icon.svg",
-        },
-        icon: [
-            // @ts-ignore
-            { url: "/icon-16x16.png", size: "16x16" },
-            { url: "/icon.svg", size: "32x32" },
-        ],
-        shortcut: "/icon.svg",
-        apple: "/icon.svg",
-        android: [
+    openGraph: {
+        title: "SolRamp: Accept crypto payment for non-crypto users on Solana",
+        description:
+            "Accept crypto payments from non-crypto users. Use our Solramp SDK on your website, available for Vuejs and Reactjs, on your WordPress site, and on your Shopify store.",
+
+        url: "https://solramp.com",
+        siteName: "Solramp",
+        images: [
             {
-                url: "/icon-192x192.svg",
+                url: "https://www.solramp.com/images/MacbookPro16.png",
+                width: 800,
+                height: 600,
+            },
+            {
+                url: "https://www.solramp.com/images/MacbookPro16.png",
+                width: 1800,
+                height: 1600,
+                alt: "Solramp Merchant Dashboard",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    icons: {
+        icon: [
+            { url: "/images/favicon/favicon-16x16.png", size: "16x16" }, //https://www.solramp.com/images/MacbookPro16.png
+            { url: "/images/favicon/favicon-32x32.png", size: "32x32" },
+        ],
+        shortcut: "/images/favicon/favicon-32x32.png",
+        apple: "/images/favicon/apple-touch-icon.png",
+        andriod: [
+            {
+                url: "/images/favicon/android-chrome-192x192.png",
                 size: "192x192",
             },
             {
-                url: "/icon-512x512.svg",
+                url: "/images/favicon/android-chrome-512x512.png",
                 size: "512x512",
             },
         ],
     },
-  }
+};
 
 //     openGraph: {
 //         title: "SolRamp: Accept crypto payment for non-crypto users on Solana",
