4dc66268cc81810cf991bbfa139ee1af37b00ea2
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-10-13 20:08:53 +0400

    List Default Express Ads

--- api/index.js
+++ api/index.js
@@ -54,6 +54,55 @@ app.get("/api/v1/business/owner", verifyToken, business.owner);
 app.post("/api/v1/business/:ref/transaction/new", business.transactionNew);
 app.get("/api/v1/business/:ref/transaction/all", business.transactionList);
 
+app.get("/api/v1/ads/express", (req, res) => {
+    return res.json({
+        status: "ok",
+        ad: [
+            {
+                currency: "NGN",
+                exchange_rate: 1045,
+                channels: {
+                    BANK: {
+                        minimum_usd: 1,
+                        maximum_usd: 10,
+                        name: "Emmanuel Joseph",
+                        bank: "GTBank",
+                        account: "0126864523",
+                        comment: "Do not include crypto in transfer comment",
+                    },
+                },
+            },
+            {
+                currency: "AED",
+                exchange_rate: 3.78,
+                channels: {
+                    BANK: {
+                        minimum_usd: 1,
+                        maximum_usd: 10,
+                        name: "Emmanuel Joseph",
+                        bank: "Mashreq Bank",
+                        account: "9828328",
+                        comment: "Do not include crypto in transfer comment",
+                    },
+                },
+            },
+            {
+                currency: "USD",
+                exchange_rate: 1,
+                channels: {
+                    WISE: {
+                        minimum_usd: 1,
+                        maximum_usd: 10,
+                        email: "iemarjay@gmail.com",
+                        name: "Emmanuel Joseph",
+                        comment: "Do not include crypto in transfer comment",
+                    },
+                },
+            },
+        ],
+    });
+});
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
