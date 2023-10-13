e710f51025799ad66b5ad205aee7ebed1b79deb4
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-10-13 20:11:03 +0400

    List Default Express Ads

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const { auth, migration, business } = require("./controllers");
+const { auth, migration, business, ads } = require("./controllers");
 const app = express();
 const port = process.env.PORT || 3000;
 const jwt = require("jsonwebtoken");
@@ -54,54 +54,7 @@ app.get("/api/v1/business/owner", verifyToken, business.owner);
 app.post("/api/v1/business/:ref/transaction/new", business.transactionNew);
 app.get("/api/v1/business/:ref/transaction/all", business.transactionList);
 
-app.get("/api/v1/ads/express", (req, res) => {
-    return res.json({
-        status: "ok",
-        ad: [
-            {
-                currency: "NGN",
-                exchange_rate: 1045,
-                channels: {
-                    BANK: {
-                        minimum_usd: 1,
-                        maximum_usd: 10,
-                        name: "Emmanuel Joseph",
-                        bank: "GTBank",
-                        account: "0126864523",
-                        comment: "Do not include crypto in transfer comment",
-                    },
-                },
-            },
-            {
-                currency: "AED",
-                exchange_rate: 3.78,
-                channels: {
-                    BANK: {
-                        minimum_usd: 1,
-                        maximum_usd: 10,
-                        name: "Emmanuel Joseph",
-                        bank: "Mashreq Bank",
-                        account: "9828328",
-                        comment: "Do not include crypto in transfer comment",
-                    },
-                },
-            },
-            {
-                currency: "USD",
-                exchange_rate: 1,
-                channels: {
-                    WISE: {
-                        minimum_usd: 1,
-                        maximum_usd: 10,
-                        email: "iemarjay@gmail.com",
-                        name: "Emmanuel Joseph",
-                        comment: "Do not include crypto in transfer comment",
-                    },
-                },
-            },
-        ],
-    });
-});
+app.get("/api/v1/ads/express", ads.express);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
