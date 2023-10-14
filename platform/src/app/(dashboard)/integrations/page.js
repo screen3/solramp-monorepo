85d3c3c47d08afb7765625bc3ac0a6c5a4ce6f95
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-10-14 18:02:15 +0400

    Fixing UI

--- platform/src/app/(dashboard)/integrations/page.js
+++ platform/src/app/(dashboard)/integrations/page.js
@@ -1,122 +1,128 @@
-'use client';
-import React from 'react';
-import { PageTitle } from '../../components/PageTitle';
-import MainComponent from '../../components/MainComponent';
-import { CopyIcon } from '../../components/IconComponent';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import Link from 'next/link';
-import { Textarea } from '@chakra-ui/react';
-import toast, { Toaster } from 'react-hot-toast';
-
+"use client";
+import React from "react";
+import { PageTitle } from "../../components/PageTitle";
+import MainComponent from "../../components/MainComponent";
+import { CopyIcon } from "../../components/IconComponent";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Link from "next/link";
+import { Textarea } from "@chakra-ui/react";
+import toast, { Toaster } from "react-hot-toast";
 
 const Integrations = () => {
-  const Text = `// SPDX-License-Identifier: MIT  
+    const Text = `<!DOCTYPE html>
+<html lang="en">
+<body>
+<button onclick="openPopup()">Toggle</button>
+<script src="popup.js"></script>
+<script>
+	/** @type {Window.Popup} */
+    let instance;
+	(() => {
+		instance = window.popup.initialize({business: "0292399023"});
+    })()
 
-    pragma solidity ^0.8.16;
-    
-    import "https://github.com/KlayOracle/klayoracle-monorepo/blob/development/oracle-contract/contracts/KlayOracleInterface.sol";
-    
-    contract OracleConsumerSample {
-      address public immutable oracleAddress;
-  
-      uint256 public klayOutput;
-  
-      constructor(address _oracleAddress) {
-          require(_oracleAddress != address(0));
-          oracleAddress = _oracleAddress;
-      }
-  
-      function swapUsdtoKlay() public returns (bool) {
-          KlayOracleInterface oracle = KlayOracleInterface(oracleAddress);
-  
-          bool replied = oracle.newOracleRequest(
-              this.swap.selector,
-              address(this)
-          );
-  
-          return replied;
-      }
-  
-      function swap(uint256 _klayOutput) external {
-          require(msg.sender == oracleAddress, "not allowed"); //ensure only Oracle contract can set price
-          klayOutput = _klayOutput;
-          //Swap usd to klay
-      }
-  }`;
+    function openPopup() {
+		instance.open({amount: "20000", fiat: "AED", token: "USDC", customer_email: "test@mailinator.com"})
+    }
+</script>
+</body>
+</html>`;
 
-  const [value, setValue] = React.useState(Text);
-  const [copied, setCopied] = React.useState(false);
-  const onChange = React.useCallback(({ target: { value } }) => {
-    setValue(value);
-    setCopied(true);
-    toast.success('Successfully copied!')
+    const [value, setValue] = React.useState(Text);
+    const [copied, setCopied] = React.useState(false);
+    const onChange = React.useCallback(({ target: { value } }) => {
+        setValue(value);
+        setCopied(true);
+        toast.success("Successfully copied!");
+    }, []);
 
-  }, []);
+    const onCopy = React.useCallback(() => {
+        setCopied(true);
+        toast.success("Successfully copied!");
+    }, []);
 
-  const onCopy = React.useCallback(() => {
-    setCopied(true);
-    toast.success('Successfully copied!')
-  }, []);
-
-  return (
-    <div>
-      <Toaster/>
-      <PageTitle title={'Integrations'} />
-
-      <MainComponent>
-        <div className=" mb-4">
-          <h3 className="text-center text-xl font-medium">Install Widget</h3>
-        </div>
-
+    return (
         <div>
-          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 py-3 lg:py-5 w-full ">
-            <div>
-              <h4>To use solramp apk,</h4>
-              <ul className="list-decimal	px-5">
-                <li>Place this code before the &lt;/body&gt; tag</li>
-                <li>
-                  To trigger with a button on your website to take payments,
-                  follow the directions on the{' '}
-                  <Link href="/" className="underline text-[#1877F2]">
-                    docs
-                  </Link>{' '}
-                  page.{' '}
-                </li>
-              </ul>
-            </div>
+            <Toaster />
+            <PageTitle title={"Integrations"} />
 
-            <div className="border border-[#DADADA] rounded-lg p-3 relative">
-              <textarea
-                onChange={onChange}
-                rows={8}
-                className="w-full py-3 px-3 resize-none focus:border-none focus-within:border-none active:border-none focus-visible:border-none focus:outline-none focus-within:outline-none active:outline-none focus-visible:outline-none"
-                value={value} readOnly
-              ></textarea>
-              {/* <Textarea
-                value={value}
-                onChange={onChange}
-                placeholder="Here is a sample placeholder"
-                size="sm"
-                resize={'none'}
-              /> */}
+            <MainComponent>
+                <div className=" mb-4">
+                    <h3 className="text-center text-xl font-medium">
+                        Install Widget
+                    </h3>
+                </div>
 
-              <CopyToClipboard
-                onCopy={onCopy}
-                options={{ message: 'Whoa!' }}
-                text={value}
-                className="text-sm rounded-lg absolute right-0 bottom-0 px-3 py-3 bg-white flex items-center gap-2 border-l border-t border-[#D0D5DD] text-[#A15DDF]"
-              >
-                <button className="">
-                  <CopyIcon />
-                  <span> {copied ? 'Copied' : 'Copy to Clipboard'}</span>
-                </button>
-              </CopyToClipboard>
-            </div>
-          </div>
+                <div>
+                    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 py-3 lg:py-5 w-full ">
+                        <div>
+                            <h4>
+                                To use solramp apk, on either your Reactjs or
+                                Vuejs App
+                            </h4>
+                            <ul className="list-decimal	px-5">
+                                <li>
+                                    Place the script before the &lt;/body&gt;
+                                    tag
+                                </li>
+                                <li>
+                                    To trigger the popup, we added a simple
+                                    button in this code snippet. You can trigger
+                                    the popup however you like either by first
+                                    accepting the order through a form, such as
+                                    the example on the solramp website or
+                                    directly pass the customer email, fiat &
+                                    token type, amount details.
+                                </li>
+                                <li>
+                                    Lastly, set your business ID in the
+                                    initialize function. You can find your
+                                    business ID in your solramp dashboard.
+                                </li>
+                                <li>
+                                    Check this{" "}
+                                    <Link
+                                        href="/"
+                                        className="underline text-[#1877F2]"
+                                    >
+                                        youtube video
+                                    </Link>{" "}
+                                    of the integration process.
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div className="border border-[#DADADA] rounded-lg p-3 relative">
+                            <textarea
+                                onChange={onChange}
+                                rows={8}
+                                className="w-full py-3 px-3 resize-none focus:border-none focus-within:border-none active:border-none focus-visible:border-none focus:outline-none focus-within:outline-none active:outline-none focus-visible:outline-none"
+                                value={value}
+                                readOnly
+                            ></textarea>
+
+                            <CopyToClipboard
+                                onCopy={onCopy}
+                                options={{ message: "Whoa!" }}
+                                text={value}
+                                className="text-sm rounded-lg absolute right-0 bottom-0 px-3 py-3 bg-white flex items-center gap-2 border-l border-t border-[#D0D5DD] text-[#A15DDF]"
+                            >
+                                <button className="">
+                                    <CopyIcon />
+                                    <span>
+                                        {" "}
+                                        {copied
+                                            ? "Copied"
+                                            : "Copy to Clipboard"}
+                                    </span>
+                                </button>
+                            </CopyToClipboard>
+                        </div>
+                    </div>
+                </div>
+            </MainComponent>
         </div>
-      </MainComponent>
-    </div>
-  );
+    );
 };
 
 export default Integrations;
--- platform/src/app/components/Forms/LandingHeroForm.jsx
+++ platform/src/app/components/Forms/LandingHeroForm.jsx
@@ -1,132 +1,152 @@
-import { Bank, Scan } from 'iconsax-react';
-import Image from 'next/image';
-import React from 'react';
+import { Bank, Scan } from "iconsax-react";
+import Image from "next/image";
+import React from "react";
 
 export default function LandingHeroForm() {
-  return (
-    <>
-      <form className="z-20">
-        <div className="flex flex-col gap-4">
-          <div>
-            <label className="font-medium mb-3" htmlFor="amount">
-              Amount
-            </label>
-            <input
-              id="amount"
-              type="text"
-              className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
-              name="amount"
-              autoComplete="off"
-              placeholder="e.g; 1000"
-            />
-          </div>
+    return (
+        <>
+            <form className="z-20">
+                <div className="flex flex-col gap-4">
+                    <div className="flex items-center justify-between gap-2">
+                        <div className="w-full md:w-1/4">
+                            <label
+                                className="font-medium mb-3"
+                                htmlFor="amount"
+                            >
+                                Amount
+                            </label>
+                            <input
+                                id="amount"
+                                type="text"
+                                className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
+                                name="amount"
+                                autoComplete="off"
+                                value="10"
+                                placeholder="e.g; 1000"
+                            />
+                        </div>
 
-          <div className="flex items-center justify-between gap-2">
-            <div className="w-full md:w-1/2">
-              <label className=" font-medium mb-3" htmlFor="curr">
-                Currency
-              </label>
-              <select
-                id="curr"
-                defaultValue={1}
-                className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
-                name="curr"
-              >
-                <option value={1}>🇦🇪 AED</option>
-                <option value={2}>🇺🇸 USD</option>
-                <option value={3}>🇳🇬 NGN</option>
-              </select>
-            </div>
-            <div className="w-full md:w-1/2">
-              <label className="font-medium mb-3" htmlFor="crypto">
-                Crypto
-              </label>
-              <select
-                id="crypto"
-                defaultValue={1}
-                className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
-                name="crypto"
-              >
-                <option value={1}>USDT</option>
-                <option value={2}>USDC</option>
-              </select>
-            </div>
-          </div>
-          <div className="flex items-center justify-between gap-2">
-            <div className="w-full md:w-1/2">
-              <label className="font-medium mb-3" htmlFor="email">
-                Email
-              </label>
-              <input
-                id="email"
-                type="email"
-                className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
-                name="email"
-                autoComplete="off"
-              />
-            </div>
-            <div className="w-full md:w-1/2">
-              <label className=" font-medium mb-3" htmlFor="address">
-                Enter recipients address
-              </label>
-              <input
-                id="address"
-                type="text"
-                className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
-                name="address"
-                autoComplete="off"
-                placeholder='HN7cABqLq.....ELLLsHHe4YWrH'
-              />
-            </div>
-          </div>
-        </div>
+                        <div className="w-full md:w-3/4">
+                            <label className="font-medium mb-3" htmlFor="email">
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                type="email"
+                                className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
+                                name="email"
+                                autoComplete="off"
+                                placeholder="e.g example@mailinator.com"
+                            />
+                        </div>
+                    </div>
 
-        <div className="flex items-center justify-between flex-wrap mt-4">
-          <div>
-            <h5 className="font-medium text-[#474545] text-sm mb-2">
-              We accept
-            </h5>
-            <div className="flex items-center justify-start text-[#959697] text-xs font-medium gap-3">
-              <div className="inline-flex items-center gap-1">
-                <Bank variant="Outline" size={15} className='text-[#A15DDF]'/>
-                Bank Transfer
-              </div>
-              <div className="inline-flex items-center gap-1">
-                <Image
-                  src={'/images/transferwise.svg'}
-                  width={12}
-                  height={12}
-                  alt="transferwise"
-                  className='w-auto h-auto'
-                />{' '}
-                Transferwise
-              </div>
-              <div className="inline-flex items-center gap-1">
-                <Scan variant="Linear" size={15} /> QR code
-              </div>
-              <div className='inline-flex items-center gap-2'>
-                <Image
-                  src={'/images/image 6.svg'}
-                  width={14}
-                  height={14}
-                  alt="image"
-                />
-                <Image
-                  src={'/images/image 7.svg'}
-                  width={14}
-                  height={14}
-                  alt="image"
-                />
-              </div>
-            </div>
-          </div>
-          <div>
-            <button className="px-10 py-3 text-white bg-[#A15DDF] rounded-[80px] ">
-              Pay now
-            </button>
-          </div>
-        </div>
-      </form>
-    </>
-  );
+                    <div className="flex items-center justify-between gap-2">
+                        <div className="w-full md:w-1/2">
+                            <label className=" font-medium mb-3" htmlFor="curr">
+                                Currency
+                            </label>
+                            <select
+                                id="curr"
+                                defaultValue={1}
+                                className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
+                                name="curr"
+                            >
+                                <option selected value={1}>
+                                    🇦🇪 AED
+                                </option>
+                                <option value={2}>🇺🇸 USD</option>
+                                <option value={3}>🇳🇬 NGN</option>
+                            </select>
+                        </div>
+                        <div className="w-full md:w-1/2">
+                            <label
+                                className="font-medium mb-3"
+                                htmlFor="crypto"
+                            >
+                                Crypto
+                            </label>
+                            <select
+                                id="crypto"
+                                defaultValue={1}
+                                className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
+                                name="crypto"
+                            >
+                                {/* <option value={1}>USDT</option> */}
+                                <option value={"usdc"}>USDC</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-between gap-2">
+                        <div className="w-full w-full">
+                            <label
+                                className=" font-medium mb-3"
+                                htmlFor="address"
+                            >
+                                Enter recipients address
+                            </label>
+                            <input
+                                id="address"
+                                type="text"
+                                className="block w-full h-12 px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 focus:border-gray-400 rounded-md focus:outline-none focus:ring-none"
+                                name="address"
+                                autoComplete="off"
+                                placeholder="HN7cABqLq.....ELLLsHHe4YWrH"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex items-center justify-between flex-wrap mt-4">
+                    <div>
+                        <h5 className="font-medium text-[#474545] text-sm mb-2">
+                            We accept
+                        </h5>
+                        <div className="flex items-center justify-start text-[#959697] text-xs font-medium gap-3">
+                            <div className="inline-flex items-center gap-1">
+                                <Bank
+                                    variant="Outline"
+                                    size={15}
+                                    className="text-[#A15DDF]"
+                                />
+                                Bank Transfer
+                            </div>
+                            <div className="inline-flex items-center gap-1">
+                                <Image
+                                    src={"/images/transferwise.svg"}
+                                    width={12}
+                                    height={12}
+                                    alt="transferwise"
+                                    className="w-auto h-auto"
+                                />{" "}
+                                Transferwise
+                            </div>
+                            <div className="inline-flex items-center gap-1">
+                                <Scan variant="Linear" size={15} /> QR code
+                            </div>
+                            <div className="inline-flex items-center gap-2">
+                                <Image
+                                    src={"/images/image 6.svg"}
+                                    width={14}
+                                    height={14}
+                                    alt="image"
+                                />
+                                <Image
+                                    src={"/images/image 7.svg"}
+                                    width={14}
+                                    height={14}
+                                    alt="image"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <button className="px-10 py-3 text-white bg-[#A15DDF] rounded-[80px] ">
+                            Pay now
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </>
+    );
 }
