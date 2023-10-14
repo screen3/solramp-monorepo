44b251d74e6ca8bd2c45546b1dffc992fae56b3f
Alofe Oluwafemi <oluwafemialofe@yahoo.com>
2023-10-14 23:05:30 +0400

    Added correct react code example to codebase

--- platform/src/app/(dashboard)/integrations/page.js
+++ platform/src/app/(dashboard)/integrations/page.js
@@ -9,26 +9,42 @@ import { Textarea } from "@chakra-ui/react";
 import toast, { Toaster } from "react-hot-toast";
 
 const Integrations = () => {
-    const Text = `<!DOCTYPE html>
-<html lang="en">
-<body>
-<button onclick="openPopup()">Toggle</button>
-<script src="popup.js"></script>
-<script>
-	/** @type {Window.Popup} */
-    let instance;
-	(() => {
-		instance = window.popup.initialize({business: "0292399023"});
-    })()
+    const reactCode = `"use client";
 
-    function openPopup() {
-		instance.open({amount: "20000", fiat: "AED", token: "USDC", customer_email: "test@mailinator.com"})
-    }
-</script>
-</body>
-</html>`;
+import Script from "next/script";
+import React, { useEffect } from "react";
 
-    const [value, setValue] = React.useState(Text);
+export default function Home() {
+
+     useEffect(() => {
+         if (typeof window !== "undefined") {
+             paymentPopup.current = window.popup.initialize({
+                 business: "123456789", //Copy your business ID from your dashboard
+             });
+         }
+     }, []);
+
+     const openPaymentPopup = () => {
+         paymentPopup.current.open({
+             amount: 10,
+             fiat: 'AED',
+             crypto: 'USDC',
+             customer_email: "customer@mailinator.com",
+         });
+     };
+     
+    return (
+        <>
+            <Script src="popup.js" strategy="beforeInteractive" />
+            <button onClick={openPaymentPopup}>Accept Fiat Payment</button>
+        </>
+    );
+}`;
+
+const vueCode = ``;
+
+    const [react, setReactCode] = React.useState(reactCode);
+    const [vue, setVueCode] = React.useState(vueCode);
     const [copied, setCopied] = React.useState(false);
     const onChange = React.useCallback(({ target: { value } }) => {
         setValue(value);
@@ -49,63 +65,67 @@ const Integrations = () => {
             <MainComponent>
                 <div className=" mb-4">
                     <h3 className="text-center text-xl font-medium">
-                        Install Widget
+                        ReactJs Example
                     </h3>
                 </div>
 
                 <div>
-                    <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 py-3 lg:py-5 w-full ">
-                        <div>
-                            <h4>
-                                To use solramp apk, on either your Reactjs or
-                                Vuejs App
-                            </h4>
-                            <ul className="list-decimal	px-5">
-                                <li>
-                                    Place the script before the &lt;/body&gt;
-                                    tag
-                                </li>
-                                <li>
-                                    To trigger the popup, we added a simple
-                                    button in this code snippet. You can trigger
-                                    the popup however you like either by first
-                                    accepting the order through a form, such as
-                                    the example on the solramp website or
-                                    directly pass the customer email, fiat &
-                                    token type, amount details.
-                                </li>
-                                <li>
-                                    Lastly, set your business ID in the
-                                    initialize function. You can find your
-                                    business ID in your solramp dashboard.
-                                </li>
-                                <li>
-                                    Check this{" "}
-                                    <Link
-                                        href="/"
-                                        className="underline text-[#1877F2]"
-                                    >
-                                        youtube video
-                                    </Link>{" "}
-                                    of the integration process.
-                                </li>
-                            </ul>
-                        </div>
-
+                    {/* grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 py-3 lg:py-5 */}
+                    <div className="w-full ">
                         <div className="border border-[#DADADA] rounded-lg p-3 relative">
                             <textarea
                                 onChange={onChange}
                                 rows={8}
-                                className="w-full py-3 px-3 resize-none focus:border-none focus-within:border-none active:border-none focus-visible:border-none focus:outline-none focus-within:outline-none active:outline-none focus-visible:outline-none"
-                                value={value}
+                                style={{ fontFamily: "monospace" }}
+                                className="w-full text-red-700 py-3 px-3 resize-none focus:border-none focus-within:border-none active:border-none focus-visible:border-none focus:outline-none focus-within:outline-none active:outline-none focus-visible:outline-none"
+                                value={react}
                                 readOnly
                             ></textarea>
 
                             <CopyToClipboard
                                 onCopy={onCopy}
                                 options={{ message: "Whoa!" }}
-                                text={value}
-                                className="text-sm rounded-lg absolute right-0 bottom-0 px-3 py-3 bg-white flex items-center gap-2 border-l border-t border-[#D0D5DD] text-[#A15DDF]"
+                                text={react}
+                                className="text-sm rounded-lg  absolute right-0 bottom-0 px-3 py-3 bg-white flex items-center gap-2 border-l border-t border-[#D0D5DD] text-[#A15DDF]"
+                            >
+                                <button className="">
+                                    <CopyIcon />
+                                    <span>
+                                        {" "}
+                                        {copied
+                                            ? "Copied"
+                                            : "Copy to Clipboard"}
+                                    </span>
+                                </button>
+                            </CopyToClipboard>
+                        </div>
+                    </div>
+                </div>
+
+                <div className=" mb-4 mt-4">
+                    <h3 className="text-center text-xl font-medium">
+                        VueJs Example
+                    </h3>
+                </div>
+
+                <div>
+                    {/* grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 py-3 lg:py-5 */}
+                    <div className="w-full ">
+                        <div className="border border-[#DADADA] rounded-lg p-3 relative">
+                            <textarea
+                                onChange={onChange}
+                                rows={8}
+                                style={{ fontFamily: "monospace" }}
+                                className="w-full text-red-700 py-3 px-3 resize-none focus:border-none focus-within:border-none active:border-none focus-visible:border-none focus:outline-none focus-within:outline-none active:outline-none focus-visible:outline-none"
+                                value={vue}
+                                readOnly
+                            ></textarea>
+
+                            <CopyToClipboard
+                                onCopy={onCopy}
+                                options={{ message: "Whoa!" }}
+                                text={vue}
+                                className="text-sm rounded-lg  absolute right-0 bottom-0 px-3 py-3 bg-white flex items-center gap-2 border-l border-t border-[#D0D5DD] text-[#A15DDF]"
                             >
                                 <button className="">
                                     <CopyIcon />
--- platform/src/app/components/sidebar.jsx
+++ platform/src/app/components/sidebar.jsx
@@ -1,113 +1,141 @@
-'use client';
-import React, { useState, useEffect, useContext } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
-import Link from 'next/link';
-import classNames from 'classnames';
-import { ArrowDown2, ArrowDown3, Buildings, Profile2User, RecordCircle } from 'iconsax-react';
-import Image from 'next/image';
-import { ArrowDown, Category } from 'react-iconly';
+"use client";
+import React, { useState, useEffect, useContext } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import Link from "next/link";
+import classNames from "classnames";
+import {
+    ArrowDown2,
+    Buildings,
+    Profile2User,
+    RecordCircle,
+    Message,
+} from "iconsax-react";
+import Image from "next/image";
+import { ArrowDown, Category } from "react-iconly";
 
 const Sidebar = () => {
-  const [isCollapsible, setIsCollapsible] = useState(true);
-  const router = useRouter();
-  const pathname = usePathname();
+    const [isCollapsible, setIsCollapsible] = useState(true);
+    const router = useRouter();
+    const pathname = usePathname();
 
-  const wrapperClasses = classNames(
-    'h-full sidebar pb-4 bg-white flex justify-between shadow-sm scrollbar-change flex-col overflow-y-auto overflow-x-hidden border-r border-[#EDEFF3] w-80'
-  );
+    const wrapperClasses = classNames(
+        "h-full sidebar pb-4 bg-white flex justify-between shadow-sm scrollbar-change flex-col overflow-y-auto overflow-x-hidden border-r border-[#EDEFF3] w-80"
+    );
 
-  const menuLinks = [
-    {
-      label: 'Dashboard',
-      href: '/dashboard',
-      icon: <Category size={20} />,
-    },
-    {
-      label: 'Transactions',
-      href: '/transactions',
-      icon: <Profile2User size={20} />,
-    },
-    {
-      label: 'Integrations',
-      href: '/integrations',
-      icon: <RecordCircle size={20} />,
-    },
-    { label: 'Support', href: '/support', icon: <RecordCircle size={20} /> },
-  ];
+    const menuLinks = [
+        {
+            label: "Dashboard",
+            href: "/dashboard",
+            icon: <Category size={20} />,
+        },
+        {
+            label: "Transactions",
+            href: "/transactions",
+            icon: <Profile2User size={20} />,
+        },
+        {
+            label: "Integrations",
+            href: "/integrations",
+            icon: <RecordCircle size={20} />,
+        },
+        {
+            label: "Support",
+            href: "mailto:info@solramp.com",
+            icon: <Message size={20} />,
+        },
+    ];
 
-  return (
-    <>
-      <div
-        className={wrapperClasses}
-        style={{
-          transition: 'width 0s ease-in-out 0s ',
-        }}
-      >
-        <div className="flex flex-col px-4">
-          <div className="flex items-center justify-center py-5 relative border-b  border-[#8D8E8E] h-20">
-            <div className="px-3 w-full block h-full">
-              <Link
-                href={'/'}
-                className="flex items-center w-full justify-center h-full"
-              >
-                <Image
-                  src="/logo/mainLogo.svg"
-                  height={40}
-                  width={30}
-                  className="transition 300ms ease object-contain w-auto h-auto"
-                  priority
-                  alt="logo"
-                />
-                <Image
-                  src="/logo/Ramp.svg"
-                  height={100}
-                  width={70}
-                  className="transition 300ms ease object-contain w-auto h-auto"
-                  priority
-                  alt="logo"
-                />
-                {/* <Logo className="transition 300ms ease" /> */}
-              </Link>
-            </div>
-          </div>
-          <nav className="mt-6 md:mt-3 grow ">
-            <div className=" flex-wrap px-4">
-              <div className="bg-[#F2F4F7] px-5 py-4 mb-3 rounded-lg">
-                <button className='flex items-start justify-between w-full'>
-                  <div className='flex items-start justify-start gap-2'>
-                    <Buildings variant="Outline" size={16} className='mt-1 text-[#A15DDF]'  />
-                    <div className='flex items-start flex-col'>
-                      <span className='text-[#11161F] text-sm font-medium'>Business name</span>
-                      <span className='text-[#909295] text-xs '>1432665</span>
+    return (
+        <>
+            <div
+                className={wrapperClasses}
+                style={{
+                    transition: "width 0s ease-in-out 0s ",
+                }}
+            >
+                <div className="flex flex-col px-4">
+                    <div className="flex items-center justify-center py-5 relative border-b  border-[#8D8E8E] h-20">
+                        <div className="px-3 w-full block h-full">
+                            <Link
+                                href={"/"}
+                                className="flex items-center w-full justify-center h-full"
+                            >
+                                <Image
+                                    src="/logo/mainLogo.svg"
+                                    height={40}
+                                    width={30}
+                                    className="transition 300ms ease object-contain w-auto h-auto"
+                                    priority
+                                    alt="logo"
+                                />
+                                <Image
+                                    src="/logo/Ramp.svg"
+                                    height={100}
+                                    width={70}
+                                    className="transition 300ms ease object-contain w-auto h-auto"
+                                    priority
+                                    alt="logo"
+                                />
+                                {/* <Logo className="transition 300ms ease" /> */}
+                            </Link>
+                        </div>
                     </div>
-                  </div>
+                    <nav className="mt-6 md:mt-3 grow ">
+                        <div className=" flex-wrap px-4">
+                            <div className="bg-[#F2F4F7] px-5 py-4 mb-3 rounded-lg">
+                                <button className="flex items-start justify-between w-full">
+                                    <div className="flex items-start justify-start gap-2">
+                                        <Buildings
+                                            variant="Outline"
+                                            size={16}
+                                            className="mt-1 text-[#A15DDF]"
+                                        />
+                                        <div className="flex items-start flex-col">
+                                            <span className="text-[#11161F] text-sm font-medium">
+                                                Business name
+                                            </span>
+                                            <span className="text-[#909295] text-xs ">
+                                                1432665
+                                            </span>
+                                        </div>
+                                    </div>
 
-                  <ArrowDown2 variant='Outline' size={12} className='text-[#81899E]'/>
-                </button>
-              </div>
-              {menuLinks.map((menuItem) => (
-                <Link
-                  key={menuItem.label}
-                  href={`${menuItem.href}`}
-                  className={`menu-item w-full font-thin ${
-                    pathname == menuItem.href ||
-                    pathname.startsWith(`${menuItem.href}/`)
-                      ? 'bg-[#F8F3FD] text-[#A15DDF]'
-                      : 'text-[#737070]'
-                  }  flex items-center py-4 px-5  my-2 rounded-lg transition-colors duration-200 ease-in hover:bg-[#F8F3FD] hover:text-[#A15DDF] justify-start text-sm`}
-                >
-                  <span className="text-left px-3">{menuItem.icon}</span>
-                  <span className={classNames('mx-2 text-sm font-normal ')}>
-                    {menuItem.label}
-                  </span>
-                </Link>
-              ))}
+                                    <ArrowDown2
+                                        variant="Outline"
+                                        size={12}
+                                        className="text-[#81899E]"
+                                    />
+                                </button>
+                            </div>
+                            {menuLinks.map((menuItem) => (
+                                <Link
+                                    key={menuItem.label}
+                                    href={`${menuItem.href}`}
+                                    className={`menu-item w-full font-thin ${
+                                        pathname == menuItem.href ||
+                                        pathname.startsWith(`${menuItem.href}/`)
+                                            ? "bg-[#F8F3FD] text-[#A15DDF]"
+                                            : "text-[#737070]"
+                                    }  flex items-center py-4 px-5  my-2 rounded-lg transition-colors duration-200 ease-in hover:bg-[#F8F3FD] hover:text-[#A15DDF] justify-start text-sm`}
+                                >
+                                    <span className="text-left px-3">
+                                        {menuItem.icon}
+                                    </span>
+                                    <span
+                                        className={classNames(
+                                            "mx-2 text-sm font-normal "
+                                        )}
+                                    >
+                                        {menuItem.label}
+                                    </span>
+                                </Link>
+                            ))}
+                        </div>
+                    </nav>
+                </div>
             </div>
-          </nav>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Sidebar;
